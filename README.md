4ccd890367c4139962dc1991868efbf1fe08c2ed
Benjamin Pochlauer <bpochlau@c4r5p3.42vienna.com>
2024-06-27 18:05:01 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mixed_messages
+# mixed messages
+
+portfolio project for my full stack course on codecademy.
+
+Goal is to program a mixed message generator in javascript.
+
---
+++ message-generator.js
@@ -0,0 +1,10 @@
+
+messages = ["Do not follow where the path may lead. Go instead where there is no path and leave a trail."];
+author = ["Ralph Waldo Emerson"]
+
+
+
+function print_message()
+{
+
+}
